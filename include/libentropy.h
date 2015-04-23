cdcdf436ca4403e1c77ad041b820ff5b17df7918
gokturk <gokturk@binghamton.edu>
2015-04-22 22:43:02 -0400

    libentropy: implement a library which abstracts entropy calculation.
    Implement libentropy which abstracts entropy calculation and provides
    a backend to calculate entropy in Shannon.
    
    User first creates an entropy context (of type struct entropy_ctx) and sets the
    ->ec_algo field to the algorithm of choice (currently only Shannon entropy
    calculation is supported), Then uses the helper function
    libentropy_update_ctx() to update the frequency of symbols and other variables
    related to entropy calculation, passing the data buffer as an argument. This
    function can be called on multiple buffers repeatedly and the context will be
    updated as if it's operating on a continuous byte stream. Finally, user calls
    libentropy_calculate(), passing the context as an argument. This function
    calls the corresponding backend entropy calculation function and sets the
    ->ec_status field to reflect the result of the operation.
    
    Signed-off-by: gokturk <gokturk@binghamton.edu>

---
+++ include/libentropy.h
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2015 Gokturk Yuksek
+ *
+ * This file is part of libentropy.
+ *
+ * libentropy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libentropy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with libentropy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef  __LIBENTROPY_H__
+#define  __LIBENTROPY_H__
+
+#include <stdlib.h>
+
+enum {
+	LIBENTROPY_ALGO_SHANNON,
+};
+
+enum {
+	LIBENTROPY_STATUS_SUCCESS,
+	LIBENTROPY_STATUS_FP_ERROR,
+	LIBENTROPY_STATUS_UNKOWN_ALGO,
+};
+
+struct entropy_ctx {
+	unsigned long long ec_freq_table[256];
+	unsigned long long ec_symbol_count;
+	unsigned long ec_algo;
+	double ec_entropy;
+	int ec_status;
+};
+
+void libentropy_update_ctx(struct entropy_ctx *ctx,
+			const void *buf, size_t buf_len);
+void libentropy_calculate(struct entropy_ctx *ctl);
+#endif /*__LIBENTROPY_H__*/
---
+++ lib/libentropy.c
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2015 Gokturk Yuksek
+ *
+ * This file is part of libentropy.
+ *
+ * libentropy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libentropy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with libentropy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "libentropy.h"
+#include <math.h>
+
+static double shannon_entropy(unsigned long long freq_table[256],
+			unsigned long long symbol_count,
+			int *err)
+{
+	double entropy = 0.0;
+	double p, logp;
+	unsigned i;
+
+	for (i = 0; i < 256; i++) {
+		/* Skip symbols with 0 frequency */
+		if (!freq_table[i])
+			continue;
+		p = (double)freq_table[i] / (double)symbol_count;
+		logp = log2(p);
+		entropy -= p * logp;
+	}
+	if (isnormal(entropy))
+		*err = LIBENTROPY_STATUS_SUCCESS;
+	else
+		*err = LIBENTROPY_STATUS_FP_ERROR;
+
+	return entropy;
+}
+
+void libentropy_update_ctx(struct entropy_ctx *ctx,
+			const void *buf, size_t buf_len)
+{
+	size_t i;
+
+	if (!buf_len)
+		return;
+
+	for (i = 0; i < buf_len; i++)
+		ctx->ec_freq_table[((unsigned char*)buf)[i]]++;
+	ctx->ec_symbol_count += buf_len;
+}
+
+void libentropy_calculate(struct entropy_ctx *ctx)
+{
+	switch (ctx->ec_algo) {
+	case LIBENTROPY_ALGO_SHANNON:
+		ctx->ec_entropy = shannon_entropy(ctx->ec_freq_table,
+						ctx->ec_symbol_count,
+						&ctx->ec_status);
+		break;
+	default:
+		ctx->ec_status = LIBENTROPY_STATUS_UNKOWN_ALGO;
+	};
+}
